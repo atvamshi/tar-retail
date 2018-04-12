8a6b6c99a4e56fdbc5f2b21b4bbc7a7d4a0080f6
vthalapu <vamsi.thalapulapalli@optum.com>
2018-04-11 21:08:11 -0500

    added configuration for getting jdbc template

---
+++ my-retail/src/main/java/com/myretail/jdbc/template/JdbcTemplateGet.java
@@ -0,0 +1,25 @@
+package com.myretail.jdbc.template;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ * Project: myretail-parent
+ * Package: com.myretail.jdbc.template
+ * <p>
+ * User: vthalapu
+ * Date: 4/11/18
+ * Time: 8:48 PM
+ * <p>
+ * Created with IntelliJ IDEA
+ * To change this template use File | Settings | File Templates.
+ */
+@Component
+public class JdbcTemplate {
+    private final Jdbc;
+
+//    @Autowired
+//    public JdbcTemplate(JdbcTemplate jdbcTemplate) {
+//        this.jdbcTemplate = jdbcTemplate;
+//    }
+}
